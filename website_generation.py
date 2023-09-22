32da716a4fa1930a6cecb08183332055cb8f2d1b
stephent2023 <124163684+stephent2023@users.noreply.github.com>
2023-09-22 15:16:34 +0100

    Update website_generation.py
    Added function calls for insert_content

--- website_generation.py
+++ website_generation.py
@@ -2,14 +2,17 @@
 import random
 from modules.scanfiles import tomlwrite, opentoml, getthemes
 from modules.generators import titleinput, gentext, genimage
-from modules.hugorun import hugoexecute, movewebfiles
+from modules.hugorun import hugoexecute, insertcontent, movewebfiles
 
 # run at the start of the program
 g_allthemes = getthemes()
 g_themeline, g_titleline, g_tomllines = opentoml()
 
+
 # run everytime a new site is generated
 g_newtitle = titleinput()
-g_themechoice = random.choice(g_allthemes)
-tomlwrite(g_themechoice, g_newtitle, g_themeline, g_titleline, g_tomllines)
+g_newtext = gentext()
+#g_themechoice = random.choice(g_allthemes)
+insertcontent(g_newtitle, g_newtext)
+tomlwrite("ananke", g_newtitle, g_themeline, g_titleline, g_tomllines)
 hugoexecute()
