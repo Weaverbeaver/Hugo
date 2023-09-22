772a06a3c9069252dff1128ad793975bfb21edd9
stephent2023 <124163684+stephent2023@users.noreply.github.com>
2023-09-22 15:29:45 +0100

    Add files via upload
    Created folder for test and added a few tests.

---
+++ tests/test_hugorun.py
@@ -0,0 +1,14 @@
+"""python modules needed"""
+from modules.hugorun import hugoexecute, insertcontent, movewebfiles
+
+
+def test_hugoexecute():
+    """ Comment for pylint, replace when test has been written."""
+
+
+def insert_content():
+    """ Comment for pylint, replace when test has been written."""
+
+
+def test_movewebfiles():
+    """ Comment for pylint, replace when test has been written."""
