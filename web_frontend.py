77f9756f7dfe840650d78d9ff259f4eda1af1f56
Andy <rodneyfunnie@hotmail.com>
2023-09-27 13:35:34 +0100

    feat: created a new frontend to generate bios

---
+++ web_frontend.py
@@ -0,0 +1,17 @@
+"""Import all the modules"""
+import os
+from apikey import apikey
+import streamlit
+from modules.generators import generate_company, generate_bio
+
+os.environ["OPENAI_API_KEY"] = apikey
+
+# Create application
+streamlit.title("Website generator")
+person = streamlit.text_input("Enter name of person")
+
+if person:
+    company = generate_company()
+    streamlit.write(company)
+    bio = generate_bio(person, company)
+    streamlit.write(bio)
