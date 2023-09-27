3c795817ba56f7bfc9c3635faf6977d774b6352c
Andy <rodneyfunnie@hotmail.com>
2023-09-27 13:45:19 +0100

    fix: module naming errors

--- website_generation.py
+++ website_generation.py
@@ -1,7 +1,7 @@
 """import things"""
 import random
 from modules.scanfiles import tomlwrite, opentoml, getthemes
-from modules.generators import titleinput  # , gentext, genimage
+from modules.generators import generate_company  # , generate_bio, generate_image
 from modules.hugorun import hugoexecute  # , movewebfiles
 
 # run at the start of the program
@@ -9,7 +9,7 @@ g_allthemes = getthemes()
 g_themeline, g_titleline, g_tomllines = opentoml()
 
 # run everytime a new site is generated
-g_newtitle = titleinput()
+g_newtitle = generate_company()
 g_themechoice = random.choice(g_allthemes)
 tomlwrite(g_themechoice, g_newtitle, g_themeline, g_titleline, g_tomllines)
 hugoexecute()
