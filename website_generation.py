0d8f18bbe1f537fb51c518155434ed1443604abf
Andy <rodneyfunnie@hotmail.com>
2023-09-27 17:08:00 +0100

    feat: refactored to use Website class

--- website_generation.py
+++ website_generation.py
@@ -1,15 +1,28 @@
 """import things"""
 import random
-from modules.scanfiles import tomlwrite, opentoml, getthemes
-from modules.generators import generate_company  # , generate_bio, generate_image
-from modules.hugorun import hugoexecute  # , movewebfiles
+from modules.scanfiles import get_themes
+from modules.generators import generate_company, generate_bio  # , generate_image
+
+# from modules.hugorun import hugoexecute  # , movewebfiles
+from modules.website import Website
+
+
+def generate_website(person, role, themes):
+    """Generates a company name for the person and role.
+    Returns a website class instance"""
+    this_website = Website()
+    this_website.read_toml()
+    this_website.update_title(generate_company())
+    this_website.update_description(
+        generate_bio(
+            thisperson=person, thisrole=role, thiscompany=this_website.toml["title"]
+        )
+    )
+    this_website.update_theme(random.choice(themes))
+    this_website.write_toml()
+    return this_website
+
 
 # run at the start of the program
-g_allthemes = getthemes()
-g_themeline, g_titleline, g_tomllines = opentoml()
-
-# run everytime a new site is generated
-g_newtitle = generate_company()
-g_themechoice = random.choice(g_allthemes)
-tomlwrite(g_themechoice, g_newtitle, g_themeline, g_titleline, g_tomllines)
-hugoexecute()
+all_themes = get_themes()
+generate_website(person="Dave Smith", role="CTO", themes=all_themes)
