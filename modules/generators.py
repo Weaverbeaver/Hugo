fb13f9f5ac51fc3c2e3e3fffb8f3478d4cb46691
Andy <rodneyfunnie@hotmail.com>
2023-09-27 09:49:48 +0100

    feat: added OpenAI title and bio generation

--- modules/generators.py
+++ modules/generators.py
@@ -1,15 +1,29 @@
 """module provides functions for taking a title inputand generating text and an image from this"""
-import random
+# import os # only used for testing API key locally
+from langchain.llms import OpenAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
+
+# os.environ["OPENAI_API_KEY"] = "api-key for testing"
+
 
 def titleinput():
     """input title"""
-    newtitle = random.choice(["Weavsite","Stevesite"])
+    llms = OpenAI(temperature=0.9)
+    newtitle = llms("Write a company name")
     return newtitle
 
 
-def gentext():
+def gentext(thiscompany):
     """generate text from title"""
-    return NotImplemented
+    llms = OpenAI(temperature=0.9)
+    bio_template = PromptTemplate(
+        input_variables=["thiscompany"],
+        template="Write a biography about the CIO of {thiscompany} to go on the company website",
+    )
+    bio_chain = LLMChain(llm=llms, prompt=bio_template)
+    bio = bio_chain.run(thiscompany)
+    return bio
 
 
 def genimage():
