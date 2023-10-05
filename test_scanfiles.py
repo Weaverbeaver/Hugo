42e4fe9de75244c350b2ee6f3da6114b05f7c150
stephent2023 <124163684+stephent2023@users.noreply.github.com>
2023-10-05 11:24:03 +0100

    Update test_scanfiles.py

--- test_scanfiles.py
+++ test_scanfiles.py
@@ -4,14 +4,17 @@ from modules.generators import titleinput, gentext, genimage
 from modules.hugorun import hugoexecute, movewebfiles
 
 def test_getthemes():
-    """tests if the themes have been retrieved"""
-    return NotImplemented
+    """ tests if the themes have been retrieved """
+    test_getthemes_obj = getthemes()
+    assert isinstance(test_getthemes_obj, list)
+    assert (len(test_getthemes_obj) >= 1)
 
 
 def test_opentoml():
-    """tests if important entries have been retrieved from hugo.toml"""
-    return NotImplemented
-
+    """ tests if important entries have been retrieved from hugo.toml """
+    test_opentoml_obj = opentoml()
+    assert isinstance(test_opentoml_obj[0], int)
+    assert isinstance(test_opentoml_obj[1], int)
 
 def test_tomlwrite():
     """Comment for pylint, replace when test has been written."""
