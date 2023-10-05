2af488ead35a476386a2ebc13bcb2e5ba89beb5f
Weaverbeaver <joe.weaver@soprasteria.com>
2023-10-05 10:59:08 +0100

    Merging files

--- test_scanfiles.py
+++ test_scanfiles.py
@@ -4,15 +4,15 @@ from modules.generators import titleinput, gentext, genimage
 from modules.hugorun import hugoexecute, movewebfiles
 
 def test_getthemes():
-    """ tests if the themes have been retrieved """
-
+    """tests if the themes have been retrieved"""
+    return NotImplemented
 
 
 def test_opentoml():
-    """ tests if important entries have been retrieved from hugo.toml """
-
+    """tests if important entries have been retrieved from hugo.toml"""
+    return NotImplemented
 
 
 def test_tomlwrite():
-    """ Comment for pylint, replace when test has been written."""
-
+    """Comment for pylint, replace when test has been written."""
+    return NotImplemented
