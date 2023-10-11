287d9a1088bb105c6f1655ae3cebd534fff96376
Weaverbeaver <123401142+Weaverbeaver@users.noreply.github.com>
2023-10-11 15:06:39 +0100

    fix: changing syntax of test_hugorun.py

--- tests/test_hugorun.py
+++ tests/test_hugorun.py
@@ -7,7 +7,7 @@ def test_hugo_execute():
     """Test if hugo has generated correct html and css files in public"""
     test_execute = hugo_execute()
     print(test_execute)
-    assert os.path.exists("public\\index.html")
+    assert os.path.exists("public/index.html")
     assert "error" not in test_execute.stdout
 
 
@@ -24,6 +24,6 @@ def test_zip_web(tmp_path):
     """Tests that the zip_web function can zip files"""
     os.mkdir(str(tmp_path)+"/tester")
     with open(str(tmp_path)+"/tester/tester.txt", 'w', encoding="utf-8"):
-        print(str(tmp_path)+"\\tester.txt")
-    zip_web(str(tmp_path),"testarchive",str(tmp_path)+"\\tester\\")
-    assert os.path.isfile(str(tmp_path)+"\\testarchive.zip")
+        print(str(tmp_path)+"/tester.txt")
+    zip_web(str(tmp_path),"testarchive",str(tmp_path)+"/tester/")
+    assert os.path.isfile(str(tmp_path)+"/testarchive.zip")
